cfc8eaa2d5c7b62d8a31c1f0a4e6e25a3a6d935f
Wilma <wilmareistad@icloud.com>
2025-11-05 11:29:18 +0100

    started with the code

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
-<main>
+<?php require __DIR__ . "/header.php" ?>
 
+<main>
+    <section>
+        <div class="campaign">
+            <img class="discount" src="phonediscount.png" alt="discount-logo">
+            <span class="campaign-text">Campaign 99SEK</span>
+        </div>
+        <img class="hero" src="https://lumiere-a.akamaihd.net/v1/images/p_thelionking_19752_1_0b9de87b.jpeg?region=0%2C0%2C540%2C810" alt="The Lion king Movie">
+        <div class="ordertickets">
+            <span>ORDER TICKETS</span>
+        </div>
+        <p>Enjoy The Lion King for only 99 SEK</p>
+    </section>
+
+    <h2>Now playing on the big screen!</h2>
+    <h2>Comming soon!</h2>
 </main>
+
+<?php require __DIR__ . "/footer.php" ?>
