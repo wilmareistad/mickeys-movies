5b4546f7a9f36b069e872f92b4cde40528f038d4
Wilma <wilmareistad@icloud.com>
2025-11-05 14:08:23 +0100

    added footer and some more html and css for index.html

--- footer.php
+++ footer.php
@@ -1,5 +1,16 @@
 <footer>
-
+    <img src="logowhite.png" alt="logo">
+    <div class="social">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/c/cd/Facebook_logo_%28square%29.png" alt="facebook">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/a/a5/Instagram_icon.png" alt="instagram">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/500px-LinkedIn_logo_initials.png" alt="linkedin">
+    </div>
+    <nav>
+        <a>Start</a>
+        <a>Tickets</a>
+        <a>About</a>
+        <a>News</a>
+    </nav>
 </footer>
 
 </body>
--- index.php
+++ index.php
@@ -1,20 +1,38 @@
 <?php require __DIR__ . "/header.php" ?>
 
 <main>
-    <section>
+    <section class="section-hero">
         <div class="campaign">
             <img class="discount" src="phonediscount.png" alt="discount-logo">
             <span class="campaign-text">Campaign 99SEK</span>
         </div>
         <img class="hero" src="https://lumiere-a.akamaihd.net/v1/images/p_thelionking_19752_1_0b9de87b.jpeg?region=0%2C0%2C540%2C810" alt="The Lion king Movie">
-        <div class="ordertickets">
+        <button class="ordertickets">
             <span>ORDER TICKETS</span>
+        </button>
+        <h3>Enjoy The Lion King for only 99 SEK</h3>
+    </section>
+    <section>
+        <h2>Now playing on the big screen!</h2>
+        <div class="playing-movies">
+            <img src="https://www.moviezine.se/wp-content/uploads/2025/02/large_12c8d2093a479ccde0e79695e879838f-open-uri20150422-12561-1dajwj_23920e88.jpeg" alt="Lilo and stitch movie">
+            <img src="https://www.moviezine.se/wp-content/uploads/2025/02/large_fe6dcc89002b0e075e9b79cb4b99c3c0-l_120917_d7ed3d0c.jpg" alt="Kejsarens nya-stil movie">
+            <img src="https://www.moviezine.se/wp-content/uploads/2025/02/large_be2f06a5403eed9b347f7f10540c9c3e-502924.jpg" alt="Lill Kycklingen movie">
         </div>
-        <p>Enjoy The Lion King for only 99 SEK</p>
+        <h2>Comming soon!</h2>
+        <div class="comming-soon">
+            <img src="https://www.moviezine.se/wp-content/uploads/2025/02/large_original-911.jpeg" alt="Tarzan Movie">
+            <img src="https://www.moviezine.se/wp-content/uploads/2025/02/large_0e0c324530767c224af685c4421d9ef6-koganget.jpg" alt="Kogänget Movie">
+            <img src="https://www.moviezine.se/wp-content/uploads/2025/02/large_5eba3f45ac8c7484fbe2775103b5738e-bjornbroder.jpg" alt="Björnbröder Movie">
+        </div>
+    </section>
+    <section class="newsletter">
+        <div>
+            <p>Join the magic – subscribe to our newsletter!</p>
+            <button>Join</button>
+        </div>
     </section>
 
-    <h2>Now playing on the big screen!</h2>
-    <h2>Comming soon!</h2>
 </main>
 
 <?php require __DIR__ . "/footer.php" ?>
