9e60bb009e2c107ce9a241a16fc4e5cc650852c8
OlofBjorn <olof.v.bjorn@gmail.com>
2025-11-26 16:58:42 +0100

    Continued work on Lion King

--- aboutlionking.php
+++ aboutlionking.php
@@ -8,111 +8,160 @@ require __DIR__."/header.php";
     <head>
         <link rel="stylesheet" href="lionstyle.css"/>
     </head>
-    <h1>
-        The Lion King
-    </h1>
+    <div id="textwrapper">
+        <h1 class=titletext>
+            The Lion King
+        </h1>
+        <section class="trailertext">
+            <p>
+                Trailer:
+            </p>
+        </section>
+    </div>
     <div  class="bgwrapper">
-        <div>
+        <div class="postandotherwrap">
             <div class="poster"> 
 
             </div>
-        </div>
-        <div class="ticketbox">
-            <div class="datebutton">
-                27 okt
-            </div>
-            <div class="datebutton">
-                30 okt
-            </div>
-            <div class="datebutton">
-                1 nov
-            </div>
-            <div class="datebutton">
-                2 nov
-            </div>
-            <div class="datebutton">
-                4 nov
-            </div>
-            <div class="datebutton">
-                7 nov
-            </div>
-            <div class="datebutton">
-                8 nov
-            </div>
-            <div class="datebutton">
-                9 nov
-            </div>
-        </div>
-        <section class="infobox1">
-            <p>
-                Disney’s epic adventure follows the story of Simba, a feisty lion cub who “just can’t wait to be king.” Led astray by his ambitious Uncle Scar, Simba adopts a carefree lifestyle with his hilarious companions, Timon and Pumbaa, and forgets his regal responsibilities. But destiny calls and he must decide when the time is right to return to the Pride Lands and reclaim his place in the “Circle of Life.”
-            </p>
-        </section>
-        <div class="infobox2">
-            <div id="ib2left">
-                <section id="duration">
-                    <div class="infotitle">
-                        Duration:
+            <div class="otherwrapper">
+                <div class="ticketbox" id="ticketmobile">
+                <p class="ticketlabel">
+                    Tickets
+                </p>
+                    <div class="datebuttons">
+                        <div class="datebutton">
+                            27 okt
+                        </div>
+                        <div class="datebutton">
+                            30 okt
+                        </div>
+                        <div class="datebutton">
+                            1 nov
+                        </div>
+                        <div class="datebutton">
+                            2 nov
+                        </div>
+                        <div class="datebutton">
+                            4 nov
+                        </div>
+                        <div class="datebutton">
+                            7 nov
+                        </div>
+                        <div class="datebutton">
+                            8 nov
+                        </div>
+                        <div class="datebutton">
+                            9 nov
+                        </div>
                     </div>
-                    <div class="infotext">
-                        1h 33m
-                    </div>
+                </div>
+                <section class="infobox1">
+                    <p class="infotext">
+                        Disney’s epic adventure follows the story of Simba, a feisty lion cub who “just can’t wait to be king.” Led astray by his ambitious Uncle Scar, Simba adopts a carefree lifestyle with his hilarious companions, Timon and Pumbaa, and forgets his regal responsibilities. But destiny calls and he must decide when the time is right to return to the Pride Lands and reclaim his place in the “Circle of Life.”
+                    </p>
                 </section>
-                <section id="releasedate">
-                    <div class="infotitle">
-                       Release Date:
-                    </div>
-                    <div class="infotext">
-                        1994
-                    </div>
-                </section>
-                                <section id="genre">
-                    <div class="infotitle">
-                        Genre:
-                    </div>
-                    <div class="infotext">
-                        Action and adventure, <br>
-                        Coming of Age, <br>
-                        Animation, <br>
-                        Musicals
-                    </div>
-                </section> 
-                <section id="rating">
-                    <div class="infotitle">
-                        Rating:
-                    </div>
-                    <div class="infotext">
-                        6+
-                    </div>
-                </section>
-            </div>
-            <div id="ib2right">
-                <section id="director">
-                    <div class="infotitle">
-                        Director:
-                    </div>
-                    <div class="infotext">
-                        Jon Favreau
-                    </div>
-                </section>
-                <section id="starring">
-                    <div class="infotitle">
-                        Starring:
-                    </div>
-                    <div class="infotext">  
-                        Donal Glover<br>
-                        Seth Rogen<br>
-                        Chiwetel Ejiofor<br>
-                        Alfre Woodard<br>
-                        Billy Eichner<br>
-                        John Kani
-                    </div>
-                </section>
-            </div>
-        </div>
-        <section class="trailerwrapper">
+                <section class="trailerwrapper" id="trailerdesktop">
 
-        </section>
+                </section>
+                <div class="ticketbox" id="ticketdesktop">
+                    <p class="ticketlabel">
+                        Tickets
+                    </p>
+                    <div class="datebuttons">
+                        <div class="datebutton">
+                            27 okt
+                        </div>
+                        <div class="datebutton">
+                            30 okt
+                        </div>
+                        <div class="datebutton">
+                            1 nov
+                        </div>
+                        <div class="datebutton">
+                            2 nov
+                        </div>
+                        <div class="datebutton">
+                            4 nov
+                        </div>
+                        <div class="datebutton">
+                            7 nov
+                        </div>
+                        <div class="datebutton">
+                            8 nov
+                        </div>
+                        <div class="datebutton">
+                            9 nov
+                        </div>
+                    </div>
+                </div>
+                <div class="infobox2">
+                    <div id="ib2left">
+                        <section id="duration">
+                            <div class="infotitle">
+                                Duration:
+                            </div>
+                            <div class="infotext">
+                                1h 33m
+                            </div>
+                        </section>
+                        <section id="releasedate">
+                            <div class="infotitle">
+                            Release Date:
+                            </div>
+                            <div class="infotext">
+                                1994
+                            </div>
+                        </section>
+                        <section id="genre">
+                            <div class="infotitle">
+                                Genre:
+                            </div>
+                            <div class="infotext">
+                                Action and adventure, <br>
+                                Coming of Age, <br>
+                                Animation, <br>
+                                Musicals
+                            </div>
+                        </section> 
+                        <section id="rating">
+                            <div class="infotitle">
+                                Rating:
+                            </div>
+                            <div class="infotext">
+                                6+
+                            </div>
+                        </section>
+                    </div>
+                    <div id="ib2right">
+                        <section id="director">
+                            <div class="infotitle">
+                                Director:
+                            </div>
+                            <div class="infotext">
+                                Jon Favreau
+                            </div>
+                        </section>
+                        <section id="starring">
+                            <div class="infotitle">
+                                Starring:
+                            </div>
+                            <div class="infotext">  
+                                Donal Glover<br>
+                                Seth Rogen<br>
+                                Chiwetel Ejiofor<br>
+                                Alfre Woodard<br>
+                                Billy Eichner<br>
+                                John Kani
+                            </div>
+                        </section>
+                    </div>
+        
+                </div>
+        </div> 
+            <section class="trailerwrapper" id="trailermobile">
+
+            </section>
+        </div>
         <section class="imageswrapper">
             <img src="/lionpics/lion1.png" class="image">
             <img src="/lionpics/lion2.png" class="image">
